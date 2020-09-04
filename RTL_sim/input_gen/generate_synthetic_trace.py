790ee5f65e7da456636d50185039c04b11e5020f
Hugo Sadok <sadok@cmu.edu>
2020-09-04 10:21:41 -0400

    Adding payload to generated trace

--- RTL_sim/input_gen/generate_synthetic_trace.py
+++ RTL_sim/input_gen/generate_synthetic_trace.py
@@ -2,11 +2,14 @@
 
 import sys
 
-from scapy.all import IP, TCP, UDP, wrpcap, Ether
+from scapy.all import IP, TCP, UDP, wrpcap, Ether, Raw
 
 
-def generate_pcap(nb_pkts, out_pcap):
+def generate_pcap(nb_pkts, out_pcap, pig=False):
     pkt = Ether()/IP(dst='192.168.1.1')/TCP(dport=80, flags='S')
+    if pig:
+        data = 'Pigs can also fly!'
+        pkt = pkt/Raw(load=data)
     pkts = []
 
     for _ in range(nb_pkts):
@@ -16,14 +19,20 @@ def generate_pcap(nb_pkts, out_pcap):
 
 
 def main():
-    if len(sys.argv) != 3:
-        print('Usage:', sys.argv[0], '[nb pkts] [output pcap]')
+    argv = sys.argv[:]
+    pig = False
+    if '--pig' in argv:
+        pig = True
+        argv.remove('--pig')
+
+    if len(argv) != 3:
+        print('Usage:', sys.argv[0], 'nb_pkts output_pcap [--pig]')
         sys.exit(1)
 
-    nb_pkts = int(sys.argv[1])
-    out_pcap = sys.argv[2]
+    nb_pkts = int(argv[1])
+    out_pcap = argv[2]
 
-    generate_pcap(nb_pkts, out_pcap)
+    generate_pcap(nb_pkts, out_pcap, pig=pig)
 
 
 if __name__ == "__main__":
