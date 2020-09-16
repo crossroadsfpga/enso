ee672ee8dd5983112e1f794b7a0f5bb23a88956b
Hugo Sadok <sadok@cmu.edu>
2020-09-16 10:55:30 -0400

    Rescanning PCIe devices after loading bitstream
    We can do this instread of rebooting the server. This is only possible
    after we specified the class code in the PCIe core.

--- hardware_test/load_bitstream.sh
+++ hardware_test/load_bitstream.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+
+VENDOR_ID="1172"
+DEVICE_ID="0000"
+
 quartus_pgm -c Intel\ Stratix\ 10\ MX\ FPGA\ Development\ Kit\ [1-12] ./load.cdf 
+
+# remove device and force rescan, this will trigger the driver's probe function
+DEVICE_ADDR=$(lspci -nn | grep ${VENDOR_ID}:${DEVICE_ID} | awk '{print $1}')
+echo "1" | sudo tee -a /sys/bus/pci/devices/0000\:${DEVICE_ADDR}/remove > /dev/null
+echo "1" | sudo tee -a /sys/bus/pci/rescan > /dev/null
