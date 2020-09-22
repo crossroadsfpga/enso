1c47dbe244a1a90530095aa077cfda82f4bfd399
Hugo Sadok <sadok@cmu.edu>
2020-09-21 21:08:12 -0400

    Avoid removing device when it is not yet present

--- hardware_test/load_bitstream.sh
+++ hardware_test/load_bitstream.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 VENDOR_ID="1172"
 DEVICE_ID="0000"
@@ -7,5 +8,7 @@ quartus_pgm -c Intel\ Stratix\ 10\ MX\ FPGA\ Development\ Kit\ [1-12] ./load.cdf
 
 # remove device and force rescan, this will trigger the driver's probe function
 DEVICE_ADDR=$(lspci -nn | grep ${VENDOR_ID}:${DEVICE_ID} | awk '{print $1}')
-echo "1" | sudo tee -a /sys/bus/pci/devices/0000\:${DEVICE_ADDR}/remove > /dev/null
+if [ -n "$DEVICE_ADDR" ]; then
+    echo "1" | sudo tee -a /sys/bus/pci/devices/0000\:${DEVICE_ADDR}/remove > /dev/null
+fi
 echo "1" | sudo tee -a /sys/bus/pci/rescan > /dev/null
