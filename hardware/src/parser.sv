4848644f893d8359b9a999534c831bb2d1a75d8a
Hugo Sadok <sadok@cmu.edu>
2021-12-13 15:38:01 -0500

    Fix format

--- hardware/src/parser.sv
+++ hardware/src/parser.sv
@@ -1,67 +1,45 @@
 `include "./constants.sv"
-/* parse the ethnet frame to data for kernel. Convert Big endian to little
-* endian */
-module parser(clk,rst,
-    disable_pcie,
-    in_pkt_data,
-    in_pkt_valid,
-    in_pkt_ready,
-    in_pkt_sop,
-    in_pkt_eop,
-    in_pkt_empty,
-    out_pkt_data,
-    out_pkt_valid,
-    out_pkt_ready,
-    out_pkt_sop,
-    out_pkt_eop,
-    out_pkt_empty,
-    in_meta_data,
-    in_meta_valid,
-    in_meta_ready,
-    out_meta_data,
-    out_meta_valid,
-    out_meta_ready
+/* 
+ * Parse the Ethernet frame to data for kernel. Convert Big endian to little
+ * endian.
+ */
+module parser(
+    input logic clk,
+    input logic rst,
+
+    input logic disable_pcie,
+
+    input  logic [511:0] in_pkt_data,
+    input  logic         in_pkt_valid,
+    output logic         in_pkt_ready,
+    input  logic         in_pkt_sop,
+    input  logic         in_pkt_eop,
+    input  logic [5:0]   in_pkt_empty,
+
+    output logic [511:0] out_pkt_data,
+    output logic         out_pkt_valid,
+    input  logic         out_pkt_ready,
+    output logic         out_pkt_sop,
+    output logic         out_pkt_eop,
+    output logic [5:0]   out_pkt_empty,
+
+    input  var metadata_t in_meta_data,
+    input  logic          in_meta_valid,
+    output logic          in_meta_ready,
+
+    output var metadata_t out_meta_data,
+    output logic          out_meta_valid,
+    input  logic          out_meta_ready
 );
-//TODO: IP header/ TCP header bigger than 5 * 32 bits
-input clk;
-input rst;
-input disable_pcie;
-input [511:0] in_pkt_data;
-input in_pkt_valid;
-output logic in_pkt_ready;
-input in_pkt_sop;
-input in_pkt_eop;
-input [5:0] in_pkt_empty;
-output logic [511:0] out_pkt_data;
-output logic out_pkt_valid;
-input  out_pkt_ready;
-output logic out_pkt_sop;
-output logic out_pkt_eop;
-output logic [5:0] out_pkt_empty;
-input metadata_t in_meta_data;
-input in_meta_valid;
-output logic in_meta_ready;
-output metadata_t out_meta_data;
-output logic out_meta_valid;
-input  out_meta_ready;
 
-/*
-typedef enum
-{
-    IDLE,
-    READ,
-    WAIT
-} state_t;
-*/
+// TODO: IP header/ TCP header bigger than 5 * 32 bits.
 
-//state_t state;
-
-
-//First flit
-logic [15:0]  eth_type;
+// First flit.
+logic [15:0] eth_type;
 logic [3:0]  ip_version;
 logic [3:0]  ip_ihl;
-//Second flit
+
+// Second flit.
 logic [5:0]  ip_dscp;
 logic [1:0]  ip_ecn;
 logic [15:0] ip_len;
@@ -73,7 +51,8 @@ logic [7:0]  ip_prot;
 logic [15:0] ip_chsm;
 logic [31:0] ip_sip;
 logic [15:0] ip_dip_high;
-//Third flit
+
+// Third flit.
 logic [15:0] ip_dip_low;
 logic [15:0] udp_sport;
 logic [15:0] udp_dport;
@@ -94,7 +73,8 @@ logic        tcp_psh;
 logic        tcp_rst;
 logic        tcp_syn;
 logic        tcp_fin;
-//Fourth flit
+
+// Fourth flit.
 logic [15:0] tcp_win;
 logic [15:0] tcp_chsm;
 logic [15:0] tcp_urgp;
@@ -103,14 +83,14 @@ metadata_t metadata;
 logic [PKT_AWIDTH-1:0] temp_pktID;
 logic support;
 
-//First flit
+// First flit.
 assign eth_type = in_pkt_data[128*3+31:128*3+16];      //16
-assign ip_version = in_pkt_data[128*3+15:128*3+12];    //4 
+assign ip_version = in_pkt_data[128*3+15:128*3+12];    //4
 assign ip_ihl = in_pkt_data[128*3+11:128*3+8];         //4
 assign ip_dscp = in_pkt_data[128*3+7:128*3+2];         //6
 assign ip_ecn = in_pkt_data[128*3+1:128*3+0];          //2
 
-//Second flit
+// Second flit.
 assign ip_len = in_pkt_data[128*2+127:128*2+112];      //16
 assign ip_id = in_pkt_data[128*2+111:128*2+96];        //16
 assign ip_flags = in_pkt_data[128*2+95:128*2+93];      //3
@@ -121,8 +101,8 @@ assign ip_chsm = in_pkt_data[128*2+63:128*2+48];       //16
 assign ip_sip = in_pkt_data[128*2+47:128*2+16];        //32
 assign ip_dip_high = in_pkt_data[128*2+15:128*2+0];    //16
 
-//Third flit
-assign ip_dip_low = in_pkt_data[128+127:128+112];  //16  
+// Third flit.
+assign ip_dip_low = in_pkt_data[128+127:128+112];  //16
 assign udp_sport = in_pkt_data[128+111:128+96];    //16
 assign udp_dport = in_pkt_data[128+95:128+80];     //16
 assign udp_len = in_pkt_data[128+79:128+64];       //16
@@ -144,7 +124,7 @@ assign tcp_rst = in_pkt_data[128+2];           //1
 assign tcp_syn = in_pkt_data[128+1];           //1
 assign tcp_fin = in_pkt_data[128+0];           //1
 
-//Fourth flit
+// Fourth flit.
 assign tcp_win = in_pkt_data[127:112];     //16
 assign tcp_chsm = in_pkt_data[111:96];     //16
 assign tcp_urgp = in_pkt_data[95:80];      //16
@@ -153,8 +133,8 @@ assign support = (eth_type == PROT_ETH) & (ip_version == IP_V4)
             & (ip_ihl == 5) & ((ip_prot == PROT_TCP) | (ip_prot == PROT_UDP));
 
 assign metadata.len = {
-    (ip_prot == PROT_TCP) ? 
-          (ip_len - (ip_ihl << 2) - (tcp_do << 2)) 
+    (ip_prot == PROT_TCP) ?
+          (ip_len - (ip_ihl << 2) - (tcp_do << 2))
         : (ip_len - (ip_ihl << 2) - 32'd8)
 }[15:0];
 assign metadata.tuple.sIP = ip_sip;
@@ -164,7 +144,10 @@ assign metadata.tuple.dPort = tcp_dport;
 assign metadata.prot = support ? ((ip_prot == PROT_TCP) ? S_TCP : S_UDP) : NS;
 assign metadata.pktID = in_meta_data.pktID;
 assign metadata.flits = in_meta_data.flits;
-assign metadata.tcp_flags = {tcp_ns,tcp_cwr,tcp_ece,tcp_urg,tcp_fack,tcp_psh,tcp_rst,tcp_syn,tcp_fin};
+assign metadata.tcp_flags = {
+    tcp_ns, tcp_cwr, tcp_ece, tcp_urg, tcp_fack, tcp_psh, tcp_rst, tcp_syn,
+    tcp_fin
+};
 assign metadata.pkt_flags = disable_pcie ? PKT_ETH : PKT_PCIE;
 assign metadata.pkt_queue_id = 0;
 assign metadata.dsc_queue_id = 0;
@@ -173,20 +156,19 @@ assign metadata.padding = 0;
 assign in_pkt_ready = out_meta_ready;
 assign in_meta_ready = out_meta_ready;
 
-//We don't output pkt anything
-//But leave the interface in case used in the future.
+// We don't output pkt anymore, but leave the interface in case used in the
+// future.
 assign out_pkt_valid = 0;
 
-
-always @ (posedge clk)
-begin
-    if(rst)begin
+always @(posedge clk) begin
+    if (rst) begin
         out_meta_valid <= 0;
     end else begin
-        //currently we only take care of the first 64 B. The options are ignored for now.
-        if( out_meta_ready &
-            in_pkt_valid & in_pkt_sop & in_pkt_eop 
-            & in_meta_valid) begin
+        // Currently we only take care of the first 64B. The options are ignored
+        // for now.
+        if (out_meta_ready & in_pkt_valid & in_pkt_sop & in_pkt_eop
+            & in_meta_valid)
+        begin
             out_meta_valid <= 1;
             out_meta_data <= metadata;
         end else begin
