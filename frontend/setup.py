8dd9db56e488c38f83576ff2412ada33024d61d6
Hugo Sadok <sadok@cmu.edu>
2022-03-13 18:08:28 -0400

    Require new netexp and tqdm

--- frontend/setup.py
+++ frontend/setup.py
@@ -43,7 +43,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'click>=8.0',
-        'netexp>=0.1.5'
+        'netexp>=0.1.7',
+        'tqdm'
     ],
     setup_requires=[
         'pytest-runner'
