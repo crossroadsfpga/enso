48a1c6aa10ca87f6f7269590a6ef8a65fd0f3175
Hugo Sadok <sadok@cmu.edu>
2022-03-19 14:20:59 -0400

    Remove all FPGAs when loading bitstream

--- hardware_test/load_bitstream.sh
+++ hardware_test/load_bitstream.sh
@@ -9,9 +9,13 @@ FPGA_NB=${1:-"1-13"}
 quartus_pgm -c "Intel Stratix 10 MX FPGA Development Kit [$FPGA_NB]" ./load.cdf
 
 # remove device and force rescan, this will trigger the driver's probe function
-DEVICE_ADDR=$(lspci -nn | grep ${VENDOR_ID}:${DEVICE_ID} | grep Ethernet | \
+DEVICE_ADDRS=$(lspci -nn | grep ${VENDOR_ID}:${DEVICE_ID} | grep Ethernet | \
     awk '{print $1}')
-if [ -n "$DEVICE_ADDR" ]; then
-    echo "1" | sudo tee -a /sys/bus/pci/devices/0000\:${DEVICE_ADDR}/remove > /dev/null
-fi
+
+for device in $DEVICE_ADDRS; do
+    if [ -n "$device" ]; then
+        echo "1" | sudo tee -a /sys/bus/pci/devices/0000\:${device}/remove \
+            > /dev/null
+    fi
+done
 echo "1" | sudo tee -a /sys/bus/pci/rescan > /dev/null
