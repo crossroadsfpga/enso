a1a67c8ab0966a3b27ace8241d2f9e6b6b515433
Hugo Sadok <sadok@cmu.edu>
2022-03-24 11:22:16 -0400

    Requiring latest netexp

--- frontend/setup.py
+++ frontend/setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'click>=8.0',
-        'netexp==0.1.8',
+        'netexp==0.1.9',
         'tqdm'
     ],
     setup_requires=[
