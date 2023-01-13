182111bc7e64814e2e9fa7d3fcb8fd270eba46e7
Hugo Sadok <sadok@cmu.edu>
2023-01-13 12:15:01 -0500

    Speed up bitstream loading

--- hardware_test/load_bitstream.sh
+++ hardware_test/load_bitstream.sh
@@ -6,7 +6,11 @@ DEVICE_ID="0000"
 
 FPGA_NB=${1:-"1-13"}
 
-quartus_pgm -c "Intel Stratix 10 MX FPGA Development Kit [$FPGA_NB]" ./load.cdf
+# We use taskset and chrt to benefit from multiple cores even when they are
+# isolated from the linux scheduler. This significantly speeds up loading the
+# bitstream. Note that we use all but the last core.
+sudo taskset -c 0-$((`nproc --all` - 2)) chrt -r 1 $(which quartus_pgm) \
+    -c "Intel Stratix 10 MX FPGA Development Kit [$FPGA_NB]" ./load.cdf
 
 # remove device and force rescan, this will trigger the driver's probe function
 DEVICE_ADDRS=$(lspci -nn | grep ${VENDOR_ID}:${DEVICE_ID} | grep Ethernet | \
