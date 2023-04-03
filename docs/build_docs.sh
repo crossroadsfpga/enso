04586e1fabfa78afb4ff57256a4705dd5986af12
Hugo Sadok <sadok@cmu.edu>
2023-04-03 18:09:42 -0400

    Cleanup doc building

---
+++ docs/build_docs.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# print commands
+set -o xtrace
+
+# exit when error occurs
+set -e
+
+ORIGINAL_PWD=$(pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR="$SCRIPT_DIR/.."
+
+HARDWARE_PATH="$REPO_DIR/hardware/src"
+HDL_DOC_PATH="$REPO_DIR/docs/hardware/modules"
+
+TEROS_HDL_COMMON_OPTS="\
+    --dep \
+    --fsm \
+    --signals only_commented \
+    --constants only_commented \
+    --process only_commented \
+    --functions only_commented \
+    --symbol_verilog / \
+    --out markdown \
+"
+
+rm -rf $HDL_DOC_PATH
+
+# Build hardware docs.
+teroshdl-hdl-documenter $TEROS_HDL_COMMON_OPTS \
+    --input $HARDWARE_PATH \
+    --outpath "$REPO_DIR/docs/out" \
+    # --recursive
+
+# Build hardware PCIe docs.
+teroshdl-hdl-documenter $TEROS_HDL_COMMON_OPTS \
+    --input "$HARDWARE_PATH/pcie" \
+    --outpath "$REPO_DIR/docs/out/pcie"
+
+mv "$REPO_DIR/docs/out/doc_internal" "$REPO_DIR/docs/hardware/modules"
+mv "$REPO_DIR/docs/out/pcie/doc_internal" "$REPO_DIR/docs/hardware/modules/pcie"
+
+cd $REPO_DIR
+mkdocs build -f mkdocs.yml
