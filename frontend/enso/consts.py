cef1e211375c7d0d366a25cd1ed9ffb3de0a7cc5
Hugo Sadok <sadok@cmu.edu>
2023-04-08 23:39:53 +0000

    Rename setup.sh to sw_setup.sh

--- frontend/enso/consts.py
+++ frontend/enso/consts.py
@@ -7,7 +7,7 @@ DEFAULT_NB_FALLBACK_QUEUES = 0
 DEFAULT_BATCH_SIZE = 64
 
 # The following paths are relative to the enso path.
-SETUP_SW_CMD = "setup.sh"
+SETUP_SW_CMD = "setup_sw.sh"
 ENSOGEN_CMD = "build/software/examples/ensogen"
 PCAPS_DIR = "frontend/pcaps/"
 PCAP_GEN_CMD = "hardware/input_gen/generate_synthetic_trace.py"
