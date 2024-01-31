d0b6071a12782f6b67e629c0a130062d3b2ba520
kaajalbgupta <kaajalg@andrew.cmu.edu>
2024-01-30 23:06:59 -0500

    changes to experiments

--- software/examples/config_experiment.cpp
+++ software/examples/config_experiment.cpp
@@ -48,27 +48,104 @@
 static volatile bool keep_running = true;
 static volatile bool setup_done = false;
 
-struct EchoArgs {
+struct ConfigArgs {
   uint32_t nb_queues;
+  uint32_t dst_ip_1;
+  uint32_t dst_ip_2;
+  uint32_t ns_interval;
+  uint32_t core_id;
+};
+
+struct EchoArgs {
   uint32_t core_id;
   uint32_t nb_cycles;
   enso::stats_t* stats;
   uint32_t application_id;
+  uint32_t nb_queues;
 };
 
 void int_handler([[maybe_unused]] int signal) { keep_running = false; }
 
 std::chrono::nanoseconds get_time() {
-  return duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+      std::chrono::system_clock::now().time_since_epoch());
+}
+
+void* change_config(void* arg) {
+  struct ConfigArgs* args = (struct ConfigArgs*)arg;
+  uint32_t nb_queues = args->nb_queues;
+  uint32_t ns_interval = args->ns_interval;
+  uint32_t core_id = args->core_id;
+
+  enso::set_self_core_id(core_id);
+
+  std::cout << "HIIIII In change config thread, running on core "
+            << sched_getcpu() << std::endl;
+
+  using enso::Device;
+  using enso::RxPipe;
+  using std::chrono;
+
+  std::unique_ptr<Device> dev = Device::Create(0, NULL);
+  std::vector<RxPipe*> pipes;
+
+  if (!dev) {
+    std::cerr << "Problem creating device" << std::endl;
+    exit(2);
+  }
+
+  uint32_t dst_ip_1 = kBaseIpAddress + core_id * 2;
+  uint32_t dst_ip_2 = kBaseIpAddress + core_id * 2 + 1;
+
+  for (uint32_t i = 0; i < nb_queues; ++i) {
+    RxPipe* pipe = dev->AllocateRxPipe();
+    if (!pipe) {
+      std::cerr << "Problem creating RX/TX pipe" << std::endl;
+      exit(3);
+    }
+    if (i % 2 == 0) {
+      pipe->Bind(kDstPort, 0, dst_ip_1, 0, kProtocol);
+    } else {
+      pipe->Bind(kDstPort, 0, dst_ip_2, 0, kProtocol);
+    }
+
+    pipes.push_back(pipe);
+  }
+
+  nanoseconds last_time = get_time();
+
+  while (keep_running) {
+    nanoseconds now = get_time();
+    bool change = false;
+    long long diff = (now - last_time).count();
+    if (diff >= ns_interval) {
+      change = true;
+      last_time = now;
+    }
+
+    int i = 0;
+    for (auto& pipe : pipes) {
+      // bind pipes to the same socket
+      if (change) {
+        if (i % 2 == 0) {
+          pipe->Bind(kDstPort, 0, dst_ip_1, 0, kProtocol);
+        } else {
+          pipe->Bind(kDstPort, 0, dst_ip_2, 0, kProtocol);
+        }
+        i++;
+      }
+    }
+  }
+  return NULL;
 }
 
 void* run_experiment(void* arg) {
   struct EchoArgs* args = (struct EchoArgs*)arg;
-  uint32_t nb_queues = args->nb_queues;
   uint32_t core_id = args->core_id;
   uint32_t nb_cycles = args->nb_cycles;
   enso::stats_t* stats = args->stats;
   uint32_t application_id = args->application_id;
+  uint32_t nb_queues = args->nb_queues;
 
   enso::set_self_core_id(core_id);
 
@@ -78,9 +155,9 @@ void* run_experiment(void* arg) {
 
   using enso::Device;
   using enso::RxPipe;
-  using namespace std::chrono;
+  using std::chrono;
 
-  Device* dev = Device::Create();
+  std::unique_ptr<Device> dev = Device::Create(application_id, NULL);
   if (!dev) {
     std::cerr << "Problem creating device" << std::endl;
     exit(2);
@@ -101,20 +178,9 @@ void* run_experiment(void* arg) {
 
   setup_done = true;
 
-  nanoseconds last_time = get_time();
-  bool change = false;
-
   while (keep_running) {
-    nanoseconds now = get_time();
-    change = false;
-    if ((now - last_time).count(); >= ms_interval) {
-      change = true;
-      last_time = now;
-    }
-
-    int i = 0;
     for (auto& pipe : pipes) {
-      auto batch = rx_pipe->RecvPkts();
+      auto batch = pipe->RecvPkts();
 
       if (unlikely(batch.available_bytes() == 0)) {
         continue;
@@ -133,20 +199,7 @@ void* run_experiment(void* arg) {
       stats->recv_bytes += batch_length;
       ++(stats->nb_batches);
 
-      rx_pipe->Clear();
-
-      // bind pipes to the same socket
-      if (change) {
-        std::cout << "change" << std::endl;
-        if (i % 2 == 0) {
-          pipe->Bind(kDstPort, 0, dst_ip_1, 0, kProtocol);
-        } else {
-          pipe->Bind(kDstPort, 0, dst_ip_2, 0, kProtocol);
-        }
-        i++;
-      } else {
-        std::cout << "no change" << std::endl;
-      }
+      pipe->Clear();
     }
   }
   return NULL;
@@ -158,34 +211,46 @@ int main(int argc, const char* argv[]) {
               << std::endl
               << std::endl;
     std::cerr << "NB_CORES: Number of cores to use." << std::endl;
-    std::cerr << "NB_QUEUES: Number of queues per core." << std::endl;
     std::cerr << "NB_CYCLES: Number of cycles to busy loop when processing each"
                  " packet."
               << std::endl;
     std::cerr << "APPLICATION_ID: Count of number of applications started "
                  "before this application, starting from 0."
               << std::endl;
+    std::cerr
+        << "NS_INTERVAL: Time between each configuration update in nanoseconds."
+        << std::endl;
     return 1;
   }
 
   uint32_t nb_cores = atoi(argv[1]);
-  uint32_t nb_queues = atoi(argv[2]);
-  uint32_t nb_cycles = atoi(argv[3]);
-  uint32_t application_id = atoi(argv[4]);
+  uint32_t nb_cycles = atoi(argv[2]);
+  uint32_t application_id = atoi(argv[3]);
+  uint32_t ns_interval = atoi(argv[4]);
 
   signal(SIGINT, int_handler);
 
   std::vector<pthread_t> threads;
   std::vector<enso::stats_t> thread_stats(nb_cores);
 
+  uint32_t nb_queues = 2;
+
+  pthread_t thread;
+  struct ConfigArgs args;
+  args.nb_queues = nb_queues;
+  args.ns_interval = ns_interval;
+  args.core_id = nb_cores;
+  pthread_create(&thread, NULL, change_config, (void*)&args);
+  threads.push_back(thread);
+
   for (uint32_t core_id = 0; core_id < nb_cores; ++core_id) {
     pthread_t thread;
     struct EchoArgs args;
-    args.nb_queues = nb_queues;
     args.core_id = core_id;
     args.nb_cycles = nb_cycles;
     args.stats = &(thread_stats[core_id]);
     args.application_id = application_id;
+    args.nb_queues = nb_queues;
     pthread_create(&thread, NULL, run_experiment, (void*)&args);
     threads.push_back(thread);
     usleep(100000);
